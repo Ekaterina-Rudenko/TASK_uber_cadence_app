775bed8a7064cbb826495586b8032c23c0b7a0d0
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-08-23 01:15:18 +0300

    Add project

--- activity-worker/src/main/java/by/rudenko/activityworker/service/impl/SaveWeatherServiceImpl.java
+++ activity-worker/src/main/java/by/rudenko/activityworker/service/impl/SaveWeatherServiceImpl.java
@@ -6,7 +6,6 @@ import by.rudenko.activityworker.repository.WeatherRepository;
 import by.rudenko.activityworker.service.SaveWeatherService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
